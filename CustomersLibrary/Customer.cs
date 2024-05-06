3c3dcbb9bb70e8f2e10cc1c1f2dae6f916189cc3
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-05-07 00:31:13 +0300

    feat: customer library and scientific achievemtn libs misc

---
+++ CustomersLibrary/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace lb4.entities.Customer;
+
+public class Customer
+{
+    public string OrganizationName { get; private set; }
+    public string ResearchTopic { get; private set; }
+    public int Price { get; private set; }
+
+    public Customer(string organizationName, string researchTopic, int price)
+    {
+        if (price < 0)
+        {
+            throw new ArithmeticException("Price is negative");
+        }
+
+        OrganizationName = organizationName;
+        ResearchTopic = researchTopic;
+        Price = price;
+    }
+}
